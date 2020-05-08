dddb3abeefd6b7587466d80fcae4095731a0d6e4
arunmathew <arunmathew1712@gmail.com>
2020-05-08 01:53:21 -0400

    Adding new

---
+++ haha.py
@@ -0,0 +1,11 @@
+import sys
+print(sys.version)
+print(sys.executable)
+
+def greetings(who):
+	greeting = "Hello, {}".format(who)
+	return greeting
+
+
+print(greetings("Arun"))
+print(greetings("World"))
